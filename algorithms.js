3b1063312639825cedaacdf7fa235d8fc33158a3
Achufam24 <achuulimagbama.gmail.com>
2022-01-11 20:39:30 +0100

    first commit

---
+++ algorithms.js
@@ -0,0 +1,45 @@
+/*var achu = ['achu','gabriel','tony','philip',];
+var i;
+for (let i = 0; i < achu.length; i++) {
+    console.log(achu[i]);   
+}
+
+alert("welcome to my page");
+
+let a = 5;
+console.log(a++);
+console.log(++a);
+
+var a = ['dog', 'cat','hen'];
+a[100] = "fox";
+console.log(a.length);
+
+function printA() {
+    console.log(answer);
+    var answer = 1;
+}; 
+
+var portfolio = {
+    fname :'achu',
+    lname : 'agbama',
+    age : 20,
+};
+console.log(portfolio.lname);
+console.log(portfolio.fname);
+console.log(portfolio.age);
+document.getElementById("demo").innerHTML = portfolio.age
+
+var achu = 5-3;
+var i = 3;
+if (i<=achu) {
+    console.log("low");
+} else{
+    console.log("high");
+}
+
+if (confirm("Hello Sir/Ma \nPress a button!")) {
+    txt = "You pressed OK!";
+} else {
+    txt = "You pressed Cancel!";
+}
+*/
---
+++ main.js
@@ -0,0 +1,29 @@
+const navMenu=document.getElementById('nav-Menu'),
+    toggleMenu=document.getElementById('toggle-Menu'),
+    closeMenu=document.getElementById('close-Menu')
+
+    toggleMenu.addEventListener('click',()=> {
+       navMenu.classList.toggle('show')
+    })
+    closeMenu.addEventListener('click',()=> {
+       navMenu.classList.remove('show')
+    })
+   //contact form validations
+     
+        // Get the modal
+      var modal = document.getElementById('id01');
+
+       // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+var person = prompt("Please enter your name",  "Harry Potter");
+
+if (person == null || person == "") {
+    txt = "Welcome to my page";
+} else {
+    txt = "Hello " + person + "! Welcome to my Page";
+}
+document.getElementById("demo").innerHTML = txt;
